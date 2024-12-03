b2812ce52b48b57fcb0da0157c4786bd1b432483
anhdh <anhdh@mobiletech.vn>
2024-12-03 15:37:35 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+function App() {
+  return (
+    <>
+      <FontAwesomeIcon icon={faSearch} />
+    </>
+  );
+}
+
+export default App;
